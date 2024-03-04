74c043e444edee2f74c9536319a43f37114ea95f
DoBinShin <runcodedb@gamil.com>
2024-03-04 22:26:10 +0900

    refactor: quiz 업로드

--- src/dobin/index.ts
+++ src/dobin/index.ts
@@ -1,1 +1,17 @@
-console.log('hello');
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(1, 2));
+
+// 타입추론 (typescript)
+const str = 1231321;
+const arr = ['1', 2];
+
+console.log(str, arr);
+
+function add(a: number, b?: number) {
+  return a + b;
+}
+
+add(1);
